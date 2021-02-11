6ee2e12eac96a8932f13c469e3f2eae25c4aafba
Rushxxx <rush.yu@gmail.com>
2021-02-11 20:37:32 +0500

    feat: production add new pic, refactor

--- js/common.js
+++ js/common.js
@@ -4,3 +4,34 @@ const menu = document.querySelector('.menu')
 menuBurger.onclick = () => {
 	menu.classList.toggle('active')
 }
+
+
+// swiper slider start
+
+const reviewSlider = new Swiper('.review-slider', {
+	// Optional parameters
+	loop: true,
+	slidesPerView: 2,
+	spaceBetween: 75,
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+		dynamicBullets: true,
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+});
+
+// swiper slider end
+
+
+// start
+
+
+// end
