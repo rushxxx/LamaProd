973c1b9c4b03dd3f877743f6c763024ed8e8b013
Rushxxx <rush.yu@gmail.com>
2021-02-12 12:33:58 +0500

    feat: add sliders on main page

--- js/common.js
+++ js/common.js
@@ -28,6 +28,24 @@ const reviewSlider = new Swiper('.review-slider', {
 
 });
 
+const casesSlider = new Swiper('.cases-slider', {
+	// Optional parameters
+	loop: true,
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+		dynamicBullets: true,
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+});
+
 // swiper slider end
 
 
